c0b418f06893ac98fefa3d064546966b9a07082a
rajat <rajatpatil468@gmail.com>
2026-04-29 22:55:40 +0530

    Prepare v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.openappslabs.fiveg"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
