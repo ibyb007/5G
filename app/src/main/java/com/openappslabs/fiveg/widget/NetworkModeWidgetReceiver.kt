a08e3efb8227dd4b3cfa7190b023c7fdb552db4e
ibyb007 <58368196+ibyb007@users.noreply.github.com>
2026-05-10 18:00:48 +0530

    Delete app/src/main/java/com/openappslabs/fiveg/widget directory

--- app/src/main/java/com/openappslabs/fiveg/widget/NetworkModeWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.openappslabs.fiveg.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class NetworkModeWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = NetworkModeWidget()
-}
