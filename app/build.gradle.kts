360ca62c7d26cc627c0372400b40b812b96b9ba2
ibyb007 <58368196+ibyb007@users.noreply.github.com>
2026-05-11 01:36:02 +0530

    Enable resource shrinking to reduce app size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            shrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
